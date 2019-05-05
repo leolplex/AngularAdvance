f5673b406f6860253084a4e5f6da5c69e8696718
Daniel Romero <raiga1234@gmail.com>
2019-05-04 22:17:54 -0500

    add metatags

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivationEnd } from '@angular/router';
 import { filter, map } from 'rxjs/operators';
+import { Title, Meta, MetaDefinition } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -9,9 +10,21 @@ import { filter, map } from 'rxjs/operators';
 })
 export class BreadcrumbsComponent implements OnInit {
   title: string;
-  constructor(private router: Router) {
+  constructor(
+    private router: Router,
+    private titleHtml: Title,
+    private meta: Meta
+  ) {
     this.getDataRoute().subscribe(event => {
       this.title = event.titulo;
+      titleHtml.setTitle(event.titulo);
+
+      const metaTag: MetaDefinition = {
+        name: 'description',
+        content: this.title
+      };
+
+      this.meta.updateTag(metaTag);
     });
   }
 
