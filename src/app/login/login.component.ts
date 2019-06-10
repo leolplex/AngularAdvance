3554070431cc5932f5e8e75eaa39275f65531fa5
Daniel Romero <raiga1234@gmail.com>
2019-06-09 19:32:22 -0500

    Create login service, login normal way works success

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { UsuarioService } from '../services/service.index';
+import { Usuario } from '../models/usuario.model';
 
 declare function initi_plugins();
 
@@ -9,13 +12,31 @@ declare function initi_plugins();
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  constructor(public router: Router) {}
+  recuerdame = false;
+  correo: string;
+
+  constructor(public router: Router, public usuarioService: UsuarioService) {}
 
   ngOnInit() {
     initi_plugins();
+    this.correo = localStorage.getItem('correo') || '';
+    if (this.correo.length > 1) {
+      this.recuerdame = true;
+    }
   }
 
-  ingresar() {
-    this.router.navigate(['/dashboard']);
+  ingresar(forma: NgForm) {
+    if (forma.invalid) {
+      return;
+    }
+
+    const usuarioLogin = new Usuario(
+      null,
+      forma.value.correo,
+      forma.value.password
+    );
+    this.usuarioService
+      .login(usuarioLogin, forma.value.recuerdame)
+      .subscribe(correcto => this.router.navigate(['dashboard']));
   }
 }
--- src/app/services/usuario/usuario.service.ts
+++ src/app/services/usuario/usuario.service.ts
@@ -4,6 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { URL_SERVICIOS } from '../../config/config';
 import { map } from 'rxjs/operators';
 import Swal from 'sweetalert2';
+import { stringify } from '@angular/core/src/render3/util';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +13,23 @@ export class UsuarioService {
   constructor(public http: HttpClient) {
     console.log('Servicio de usuario listo');
   }
+  login(usuario: Usuario, recordar: boolean = false) {
+    if (recordar) {
+      localStorage.setItem('correo', usuario.correo);
+    } else {
+      localStorage.removeItem('correo');
+    }
+
+    const url = URL_SERVICIOS + '/login';
+    return this.http.post(url, usuario).pipe(
+      map((resp: any) => {
+        localStorage.setItem('id', resp.id);
+        localStorage.setItem('token', resp.token);
+        localStorage.setItem('usuario', JSON.stringify(resp.usuario));
+        return true;
+      })
+    );
+  }
 
   crearUsuario(usuario: Usuario) {
     const url = URL_SERVICIOS + '/usuario';
@@ -19,7 +37,7 @@ export class UsuarioService {
       map((resp: any) => {
         Swal.fire({
           title: 'Usuario creado',
-          text:  usuario.correo,
+          text: usuario.correo,
           type: 'success',
           confirmButtonText: 'ok'
         });
